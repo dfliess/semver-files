6de918606877fcdfda9c51131d159ce811ed9133
Francisek <francisek.is@free.fr>
2017-11-09 10:11:34 +0100

    Add eslint rules and apply them

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  'extends': 'airbnb-base',
+  'env': {
+    'node': true,
+    'mocha': true
+  },
+  'rules': {
+    'no-console': [1, {
+      'allow': ['warn']
+    }],
+    'comma-dangle': ['error', {
+      'functions': 'never',
+      'arrays' : 'ignore',
+      'objects': 'ignore',
+      'exports': 'never',
+      'imports': 'never'
+    }],
+    'import/no-unresolved': ['error', {
+      ignore: ['\/config\/notifier\.js$']
+    }]
+  },
+  'overrides': [
+    {
+      'files': ['test/**/*.js'],
+      'rules': {
+        'import/no-extraneous-dependencies': ["error", {"devDependencies": true}],
+        'import/no-dynamic-require':'off',
+        'global-require': 'off'
+      }
+    }
+  ]
+};
--- test/lib/version/enumerate.js
+++ test/lib/version/enumerate.js
@@ -1,51 +1,50 @@
-const should = require('should');
+require('should');
+
 const basedir = `${__dirname}/../../..`;
 
-describe('lib/version/upgrade/enumerate', function () {
+describe('lib/version/upgrade/enumerate', () => {
   const Enumerate = require(`${basedir}/lib/version/enumerate.js`);
 
-  describe('#all', function () {
-
-    it('should return an empty array for invalid directory', function () {
+  describe('#all', () => {
+    it('should return an empty array for invalid directory', () => {
       const enumerate = new Enumerate('test/fixtures/version/invalid');
-        return enumerate.all().should.be.eql([]);
+      return enumerate.all().should.be.eql([]);
     });
 
-    it('should return an empty array if no version in directory', function () {
-        const enumerate = new Enumerate('test/fixtures/version');
-        return enumerate.all()
-            .should.be.eql([]);
-    });
-      
-    it('should return an array of reverse sorted version files and not be recursive', function () {
-        const enumerate = new Enumerate('test/fixtures/version/upgrade');
-        return enumerate.all()
-            .should.be.eql(['1.0.1', '1.0.0']);
+    it('should return an empty array if no version in directory', () => {
+      const enumerate = new Enumerate('test/fixtures/version');
+      return enumerate.all()
+        .should.be.eql([]);
     });
 
+    it('should return an array of reverse sorted version files and not be recursive', () => {
+      const enumerate = new Enumerate('test/fixtures/version/upgrade');
+      return enumerate.all()
+        .should.be.eql(['1.0.1', '1.0.0']);
+    });
   });
 
-  describe('#latest', function () {
-    it('should return the highest version', function () {
+  describe('#latest', () => {
+    it('should return the highest version', () => {
       const enumerate = new Enumerate('test/fixtures/version/upgrade');
       enumerate.latest().should.be.eql('1.0.1');
     });
   });
 
-  describe('#first', function () {
-    it('should return the lowest version', function () {
+  describe('#first', () => {
+    it('should return the lowest version', () => {
       const enumerate = new Enumerate('test/fixtures/version/upgrade');
       enumerate.first().should.be.eql('1.0.0');
     });
   });
 
-  describe('#map', function () {
-    it('should apply callback on all version items in Enumerate instance context', function () {
+  describe('#map', () => {
+    it('should apply callback on all version items in Enumerate instance context', () => {
       const enumerate = new Enumerate('test/fixtures/version/upgrade');
-      enumerate.map(function (item) {
-        return `passed ${item} ${this.latest()}`
+      enumerate.map(function formatCallback(item) {
+        return `passed ${item} ${this.latest()}`;
       })
-      .should.be.eql(['passed 1.0.1 1.0.1', 'passed 1.0.0 1.0.1']);
+        .should.be.eql(['passed 1.0.1 1.0.1', 'passed 1.0.0 1.0.1']);
     });
   });
 });
